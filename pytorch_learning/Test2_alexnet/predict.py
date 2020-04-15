0340ba547905f47559523973d53b1de364ed16ed
Feuerwerk99 <Feuerwerk99@users.noreply.github.com>
2020-04-15 16:44:57 +0800

    Update predict.py

--- pytorch_learning/Test2_alexnet/predict.py
+++ pytorch_learning/Test2_alexnet/predict.py
@@ -4,19 +4,12 @@ from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import json
+import os
 
 data_transform = transforms.Compose(
     [transforms.Resize((224, 224)),
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-# load image
-img = Image.open("../tulip.jpg")
-plt.imshow(img)
-# [N, C, H, W]
-img = data_transform(img)
-# expand batch dimension
-img = torch.unsqueeze(img, dim=0)
 
 # read class_indict
 try:
@@ -31,11 +24,27 @@ model = AlexNet(num_classes=5)
 # load model weights
 model_weight_path = "./AlexNet.pth"
 model.load_state_dict(torch.load(model_weight_path))
-model.eval()
-with torch.no_grad():
-    # predict class
-    output = torch.squeeze(model(img))
-    predict = torch.softmax(output, dim=0)
-    predict_cla = torch.argmax(predict).numpy()
-print(class_indict[str(predict_cla)], predict[predict_cla].item())
-plt.show()
+
+test_path = "../../data_set/flower_data/test/"
+images_test = os.listdir(test_path)
+
+for image in images_test:
+    # load image
+    img = Image.open(test_path + image)
+
+    # plt.imshow(img)
+    # [N, C, H, W]
+
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img))
+        predict = torch.softmax(output, dim=0)
+        # Returns the indices of the maximum value of all elements in the input tensor.
+        predict_cla = torch.argmax(predict).numpy()
+    print(class_indict[str(predict_cla)], predict[predict_cla].item())
+
